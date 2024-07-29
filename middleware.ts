dcc7daa77080262590333d4912a689b3bdd4b085
liman-ai <1993limanai@gmail.com>
2024-07-29 20:52:44 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { rateLimitMiddleware } from './middleware/rateLimit';
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 
-export default async function middleware(req: NextRequest) {
-  // Rate limiting kontrolü
-  const rateLimitResponse = await rateLimitMiddleware(req);
-  if (rateLimitResponse.status === 429) {
-    return rateLimitResponse;
-  }
+export default NextAuth(authConfig).auth;
 
-  // Kimlik doğrulama kontrolü
-  const authResponse = await NextAuth(authConfig).auth(req, NextResponse.next());
-  if (authResponse.status === 401) {
-    return authResponse;
-  }
-
-  return authResponse;
-}
-
-// Middleware'in uygulanacağı yolları belirleyin
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)', '/api/query'],
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
 };
