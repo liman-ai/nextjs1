3375c5ba45f00dd52e45c43c3f4cf3a8f318117c
liman-ai <1993limanai@gmail.com>
2024-07-29 20:15:18 +0300

    Create rateLimit.ts

---
+++ middleware/rateLimit.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { getQueryCount, setQueryCountWithTTL } from '../lib/kvUtils';
+
+const QUERY_LIMIT = 1; // Günlük sorgu limiti
+const DAY_IN_SECONDS = 86400; // 24 saat
+
+export async function rateLimitMiddleware(req: NextRequest) {
+  const userKey = req.ip;
+
+  const queryCount = await getQueryCount(userKey);
+  
+  if (queryCount >= QUERY_LIMIT) {
+    return new Response('Rate limit exceeded', { status: 429 });
+  } else {
+    await setQueryCountWithTTL(userKey, queryCount + 1, DAY_IN_SECONDS);
+  }
+
+  return NextResponse.next();
+}
