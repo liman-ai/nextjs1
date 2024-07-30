57810e1dbc630e45fb6ea5aa77e39ac47a96a43f
liman-ai <1993limanai@gmail.com>
2024-07-30 12:25:00 +0300

    Update page.tsx

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,32 +1,22 @@
-import { nanoid } from '@/lib/utils';
-import { Chat } from '@/components/chat';
-import { AI } from '@/lib/chat/actions';
-import { auth } from '@/auth';
-import { Session } from '@/lib/types';
-import { getMissingKeys } from '@/app/actions';
-import { toast } from 'sonner'; // Bu kütüphaneyi hata bildirimleri için kullanacağız
+import { nanoid } from '@/lib/utils'
+import { Chat } from '@/components/chat'
+import { AI } from '@/lib/chat/actions'
+import { auth } from '@/auth'
+import { Session } from '@/lib/types'
+import { getMissingKeys } from '@/app/actions'
 
 export const metadata = {
   title: 'Next.js AI Chatbot'
-};
+}
 
 export default async function IndexPage() {
-  const id = nanoid();
-  const session = (await auth()) as Session;
-  const missingKeys = await getMissingKeys();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-     
-    } catch (error) {
-      toast.error(`Error: ${error.message}`);
-    }
-  };
+  const id = nanoid()
+  const session = (await auth()) as Session
+  const missingKeys = await getMissingKeys()
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
-      <Chat id={id} session={session} missingKeys={missingKeys} onSubmit={handleSubmit} />
+      <Chat id={id} session={session} missingKeys={missingKeys} />
     </AI>
-  );
+  )
 }
